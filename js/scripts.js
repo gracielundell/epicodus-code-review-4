504aa94eeb4853ff45c3a3a8afa11ebd1f193a10
Gracie Lundell <gracielundell@gmail.com>
2016-01-29 10:08:45 -0800

    Apply new stratagy for getting pizza info

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,37 @@
-function Pizza (topping, size) {
+function Pizza (topping, size, numberOfPizzas) {
   this.topping = topping;
-  this.size = size;
+  this.pizzaSize = size;
+  this.numberOfPizzas = numberOfPizzas;
 };
 
-Pizza.prototype.addSize = function() {
-  var total = 5;
-  if (this.size === "5") { //small
-    total = total;
-  } else if (this.size === "6") { //med
-    total += 1;
-  } else if (this.size === "7") { //large
-    total += 2;
-  } else if (this.size === "8") { //fs
-    total += 3;
-  }
-  return total;
-};
+// Pizza.prototype.addSize = function() {
+//   var total = 5;
+//   if (this.pizzaSize === "5") { //small
+//     total = total;
+//   } else if (this.pizzaSize === "6") { //med
+//     total += 1;
+//   } else if (this.pizzaSize === "7") { //large
+//     total += 2;
+//   } else if (this.pizzaSize === "8") { //fs
+//     total += 3;
+//   }
+//   return total;
+// };
 
-Pizza.prototype.addToppings = function() {
-  var total = this.addSize;
-  if (this.topping === "Olives" || this.topping === "Mushrooms" || this.topping === "Pinapple" ||) {
-    total += 2;
-  }
-}
+Pizza.prototype.fullPizza = function() {
+  var total = 0;
+  for (var i = 0; i < this.topping; i++) {
+    total += 2; //for every topping, add $3 onto total
+  };
+
+  if (this.pizzaSize === "small") {
+    total += 5;
+  };
+  if (this.pizzaSize === "medium") {
+    total += 10;
+  };
+  if (this.pizzaSize === "large") {
+    total += 15;
+  };
+  return total * this.numberOfPizzas; //return order total for two identical pizzas
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,13 @@
 describe("Pizza", function() {
-  it("will initialize a pizza with default topping", function() {
-    var testPizza = new Pizza("olives", "small");
+  it("will initialize a pizza", function() {
+    var testPizza = new Pizza("olives", "small", "2");
     expect(testPizza.topping).to.equal("olives");
-    expect(testPizza.size).to.equal("small");
+    expect(testPizza.pizzaSize).to.equal("small");
+    expect(testPizza.numberOfPizzas).to.equal("2");
   });
 
   it("will calculate price for a full pizza", function() {
     var testPizza = new Pizza("olives", "small")
     expect(testPizza.fullPizza).to.equal(7);
   });
-
 });
