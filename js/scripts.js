53dab9409b3a9f029fb78ff14b42e95ef8ef6afc
Gracie Lundell <gracielundell@gmail.com>
2016-01-29 08:38:16 -0800

    Add toppings prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
+function Pizza (topping, size) {
+  this.topping = topping;
+  this.size = size;
+};
 
+Pizza.prototype.addSize = function() {
+  var total = 5;
+  if (this.size === "5") { //small
+    total = total;
+  } else if (this.size === "6") { //med
+    total += 1;
+  } else if (this.size === "7") { //large
+    total += 2;
+  } else if (this.size === "8") { //fs
+    total += 3;
+  }
+  return total;
+};
+
+Pizza.prototype.addToppings = function() {
+  var total = this.addSize;
+  if (this.topping === "Mlives" || this.topping === "Mushrooms" || this.topping === "Pinapple" ||) {
+    total += 2;
+  }
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,12 @@
-describe("HangMan", function() {
-  it("", function() {
-    expect().to.equal();
+describe("Pizza", function() {
+  it("will initialize a pizza with default topping", function() {
+    var testPizza = new Pizza("olives", "small");
+    expect(testPizza.topping).to.equal("olives");
+    expect(testPizza.size).to.equal("small");
   });
+
+  // it("will allow multiple topping choices to be applied", function() {
+  //   var testPizza = new Pizza("")
+  // })
+
 });
